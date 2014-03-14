3b21eaec2765e87d00dfeb6922c02717e599b524
Logan Hasson <logan.hasson@gmail.com>
2014-03-14 15:04:25 -0400

    Almost

--- spec/002.ORM/crowd_fundr_campaign.rb
+++ spec/002.ORM/crowd_fundr_campaign.rb
@@ -2,4 +2,13 @@ class CrowdFundrCampaign < ActiveRecord::Base
   def __
     raise "Replace the __ with code to make the test pass"
   end
+
+  def self.funded_campaigns
+    where("funding_goal <= total_funding")
+  end
+
+  def self.book_campaigns
+    binding.pry
+    where("name LIKE '%Book%'")
+  end
 end
